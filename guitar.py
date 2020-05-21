f65d6da222f07738e71b8526712634cd3692f5bc
WilliamLynn2002 <lynnmyataung@my.jcu.edu.au>
2020-05-21 19:12:12 +0630

    Add name, year and cost.

---
+++ guitar.py
@@ -0,0 +1,17 @@
+
+""" Github Link - """
+
+class Guitar:
+    def __init__(self, name="", year=0, cost=0):
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self):
+        return "{} ({}): ${}.".format(self.name, self.year, self.cost)
+
+    def get_age(self):
+        return 2018 - self.year
+
+    def is_vintage(self):
+        return 2018 - self.year >= 50
